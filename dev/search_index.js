a6a0d2f7445e0a2865ca49630ba70351df28935d
Documenter.jl <documenter@juliadocs.github.io>
2022-05-13 13:23:39 +0000

    build based on e5f9fc5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridGraphs","category":"page"},{"location":"#GridGraphs","page":"Home","title":"GridGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GridGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridGraphs]","category":"page"}]
+}
